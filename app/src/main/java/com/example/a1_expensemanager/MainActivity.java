7464e380d8408d6cbd9e4fda4d5df6e08c000854
Srivishnu MV <srivishnum.v06@gmail.com>
2023-06-29 16:48:27 +0530

    Signup Update 4

--- app/src/main/java/com/example/a1_expensemanager/MainActivity.java
+++ app/src/main/java/com/example/a1_expensemanager/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
     }
 }
