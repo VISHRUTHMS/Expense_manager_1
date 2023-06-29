c6bc91a2dfb9057a55dbc983c3181a18ca543c2e
Srivishnu MV <srivishnum.v06@gmail.com>
2023-06-29 15:01:34 +0530

    SignUp_Login_Java_Part1

--- app/src/main/java/com/example/a1_expensemanager/LoginActivity.java
+++ app/src/main/java/com/example/a1_expensemanager/LoginActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.a1_expensemanager.databinding.ActivityMainBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
--- app/src/main/java/com/example/a1_expensemanager/SignUpActivity.java
+++ app/src/main/java/com/example/a1_expensemanager/SignUpActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.example.a1_expensemanager.databinding.ActivityMainBinding;
+import com.example.a1_expensemanager.databinding.ActivitySignUpBinding;
 
+public class SignUpActivity extends AppCompatActivity {
+    ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding=ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
