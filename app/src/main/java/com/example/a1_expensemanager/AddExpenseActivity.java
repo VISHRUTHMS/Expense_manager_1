5d8d9091a2083a71744b944d1cbe95876757ff8e
VISHRUTHMS <vishruth.ms@gmail.com>
2023-06-29 15:18:29 +0530

    Updated Till 20Min Video2

--- app/src/main/java/com/example/a1_expensemanager/AddExpenseActivity.java
+++ app/src/main/java/com/example/a1_expensemanager/AddExpenseActivity.java
@@ -1,8 +1,12 @@
 package com.example.a1_expensemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.a1_expensemanager.databinding.ActivityAddExpenseBinding;
 
@@ -16,6 +20,46 @@ public class AddExpenseActivity extends AppCompatActivity {
 
         binding = ActivityAddExpenseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.add_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.saveExpense)
+        {
+            createExpense();
+            return true;
+        }
+        return false;
+    }
+
+    private void createExpense() {
+
+        String amount=binding.amount.getText().toString();
+        String note=binding.note.getText().toString();
+        String category=binding.category.getText().toString();
+
+        boolean incomeChecked= binding.incomeRadio.isChecked();
+
+
+       // Firebase
+
+
+        if(amount.trim().length()==0)
+        {
+            binding.amount.setError("Empty");
+            return;
+        }
+
 
     }
 }
