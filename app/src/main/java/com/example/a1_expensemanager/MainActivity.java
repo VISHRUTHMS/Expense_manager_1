002896d2b406e7bfc82e0b9bf34fb7dd027e6120
Srivishnu MV <srivishnum.v06@gmail.com>
2023-06-29 16:36:04 +0530

    Signup Update 3

--- app/src/main/java/com/example/a1_expensemanager/MainActivity.java
+++ app/src/main/java/com/example/a1_expensemanager/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
